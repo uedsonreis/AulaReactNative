800a169eb7c71c264ed7047116576de0a557313d
Uedson Reis <uedsonreis@gmail.com>
2020-03-19 18:57:10 -0300

    Fix a bug.

--- App.tsx
+++ App.tsx
@@ -2,10 +2,10 @@ import React, { Component, ReactNode } from 'react';
 
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import { HomeScreen } from './pages/Home';
-import { ResultScreen } from './pages/Result';
+import { HomeScreen } from './src/pages/Home';
+import { ResultScreen } from './src/pages/Result';
 
-export default class App extends Component {
+export default class App extends Component<any, any> {
 
     render(): ReactNode {
 
